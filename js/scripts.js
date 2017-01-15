ea08837751e9533b1cb4e6f94fa1362296361772
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-15 23:03:22 +0100

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿function Telefon(marka, model, cena, kolor, pojemnosc) {
+	this.marka = marka;
+	this.model = model;
+	this.cena = cena;
+	this.kolor = kolor;
+	this.pojemnosc = pojemnosc;
+}
+Telefon.prototype.printInfo = function() {
+	console.log("Marka telefonu to " + this.marka + ", model " + this.model + ", pojemność to " + this.pojemnosc + ", kolor to " + this.kolor + ", a cena to " + this.cena + "zł.");
+}
+
+var iPhone6S = new Telefon("Apple", "iPhone6S", 2250, "srebrny", "16GB");
+var SamsungGalaxyS7 = new Telefon("Samsung", "Galaxy S7", 3299, "czarny", "32GB");
+var OnePlusOne = new Telefon("OnePlus", "One", 1600, "biały", "32GB");
+
+iPhone6S.printInfo();
+SamsungGalaxyS7.printInfo();
+OnePlusOne.printInfo();
