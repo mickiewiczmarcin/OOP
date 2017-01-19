871f7fb99f8d2c5736375cc94e9a6e577c012306
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-19 22:48:55 +0100

    add Warranty Cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,16 @@
 	this.cena = cena;
 	this.kolor = kolor;
 	this.pojemnosc = pojemnosc;
+	}
+Telefon.prototype.getWarrantyCost = function(price) {
+	return price * 0.1
 }
 Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka + ", model " + this.model + ", pojemność to " + this.pojemnosc + ", kolor to " + this.kolor + ", a cena to " + this.cena + "zł.");
+	console.log("Marka telefonu to " + this.marka + ", model " + this.model + ", pojemność to " + this.pojemnosc + ", kolor to " + this.kolor + ", a cena to " + this.cena + "zł. Cena rozszerzenia gwarancji to " + this.getWarrantyCost(this.cena) + "zł.");
 }
-
 var iPhone6S = new Telefon("Apple", "iPhone6S", 2250, "srebrny", "16GB");
-var SamsungGalaxyS7 = new Telefon("Samsung", "Galaxy S7", 3299, "czarny", "32GB");
+var SamsungGalaxyS7 = new Telefon("Samsung", "Galaxy S7", 3300, "czarny", "32GB");
 var OnePlusOne = new Telefon("OnePlus", "One", 1600, "biały", "32GB");
-
 iPhone6S.printInfo();
 SamsungGalaxyS7.printInfo();
 OnePlusOne.printInfo();
